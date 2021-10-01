d5b76b4a5a82705aec329f37457324455e6ef1a3
mehedihasanwd <hellomehediwd@gmail.com>
2021-10-02 04:11:07 +0600

    Updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Shop></Shop>
+      <h3>Hello</h3>
     </div>
   );
 }
